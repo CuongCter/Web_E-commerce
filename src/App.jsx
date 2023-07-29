11718dff045733e0310b2a77b7151b3b2479e826
CuongCter <cuongcter442002@gmail.com>
2023-07-29 09:51:38 +0700

    :zap: coding custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 function App() {
 
   return (
     <div className="App">
     <Routes>
       <Route path='/login' element={<Login></Login>}/>
+      <Route path='/signup' element={<SignUp></SignUp>}/>
+        <Route path='/forgot-password' element={<ForgotPassword></ForgotPassword>}></Route>
     </Routes>
   </div>
   )
