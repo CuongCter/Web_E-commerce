280a5560265115edb193ca971b5e24e40db22faf
minhnguyeenx <minhhlslvp@gmail.com>
2023-08-14 14:11:20 +0700

    :memo: coding admin

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,16 @@ import ShoppingCart from './pages/ShoppingCart';
 import CheckOut from './pages/CheckOut';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Admin from './admin/Admin';
 import ForgotPassword from './pages/ForgotPassword';
 import User from './pages/User';
+import Products from './admin/components/pages/Products';
+import LayoutAdmin from './admin/components/Layout/LayoutAdmin';
+import MainDash from './admin/components/MainDash/MainDash';
+import Orders from './admin/components/pages/Orders';
+import Customers from './admin/components/pages/Customers';
+import { Category } from '@mui/icons-material';
+import CategoryQL from './admin/components/pages/CategoryQL';
 
 function App() {
   return (
@@ -27,13 +35,13 @@ function App() {
         <Route path='/login' element={<Login></Login>}/>
         <Route path='/signup' element={<SignUp></SignUp>}/>
         <Route path='/forgot-password' element={<ForgotPassword></ForgotPassword>}></Route>
-        {/* <Route  element= {<Admin></Admin>}>
+        <Route  element= {<Admin></Admin>}>
             <Route path='/admin' element={<MainDash></MainDash>}></Route>
             <Route path='/orders' element={<Orders></Orders>}></Route>
             <Route path='/customers' element={<Customers></Customers>}></Route>
             <Route path='/products' element={<Products></Products>}></Route>
             <Route path='/categoryql' element={<CategoryQL></CategoryQL>}></Route>
-        </Route> */}
+        </Route>
       </Routes>
     </div>
   );
